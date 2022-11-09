0124dd98ccc214172d6f71f689010bc9e4a8a9b5
Success Imamun <success.imamun@decagon.dev>
2022-11-09 16:14:06 +0100

    End of session two

--- NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -10,7 +10,7 @@ namespace NZWalks.API.Data
 
         }
 
-        public DbSet<Region> Region { get; set; }
+        public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
         public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
     }
